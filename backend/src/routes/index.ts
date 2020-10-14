45e273f1052bc0d72d3c879a8e1258f3aa42f654
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2020-10-13 21:34:02 -0300

    create orphanage

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,16 +1,36 @@
 import { Router } from 'express';
+import { getRepository } from 'typeorm';
+
+import Orphanage from '../models/Orphanage';
 
 const routes = Router();
 
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
+routes.post('/orphanages', async (request, response) => {
+  const {
     name,
-    email,
-  };
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
 
-  return response.json(user);
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return response.status(201).json(orphanage);
 });
 
 export default routes;
