270e3194415ed58887a40988712198287bc834ef
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2020-10-16 21:58:23 -0300

    eslint prettier and editor config

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, } from 'react-native';
 import  MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler'
 
 import mapMarker from '../images/map-marker.png';
 
@@ -18,7 +19,7 @@ export default function OrphanagesMap() {
   }
   return (
       <View style={styles.container}>
-    <MapView 
+    <MapView
       provider={PROVIDER_GOOGLE}
       style={styles.map}
       initialRegion={{
@@ -26,20 +27,20 @@ export default function OrphanagesMap() {
         longitude: -46.7382092,
         latitudeDelta: 0.008,
         longitudeDelta: 0.008,
-      }}  
+      }}
     >
       <Marker
         icon={mapMarker}
-        calloutAnchor={{ 
+        calloutAnchor={{
           x: 2.7,
           y: 0.8,
         }}
-        coordinate={{ 
+        coordinate={{
           latitude: -23.2861537,
           longitude: -46.7382092,
         }}
       >
-        <Callout tooltip onPress={handleNavigationToOrphanageDetail}> 
+        <Callout tooltip onPress={handleNavigationToOrphanageDetail}>
           <View style={styles.calloutContainer}>
           <Text style={styles.calloutText}>Orfanato teste</Text>
           </View>
@@ -50,9 +51,9 @@ export default function OrphanagesMap() {
     <View style={styles.footer}>
       <Text style={styles.footerText}>2 Orfanatos encontrados</Text>
 
-      <TouchableOpacity style={styles.createOrphanageButton} onPress={handleNavigationToCreateOrphanage}>
+      <RectButton style={styles.createOrphanageButton} onPress={handleNavigationToCreateOrphanage}>
         <Feather name="plus" size={20} color="#FFF"/>
-      </TouchableOpacity>
+      </RectButton>
     </View>
   </View>
   )
--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -1,29 +1,41 @@
-import React from 'react'
+import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack'
+import { createStackNavigator } from '@react-navigation/stack';
 
-const { Navigator, Screen } = createStackNavigator();
-
+import Header from './components/Headers';
 import OrphanagesMap from './pages/OrphanagesMap';
-import OrphanagesDetail from './pages/OrphanageDetails'
+import OrphanagesDetail from './pages/OrphanageDetails';
 
 import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
 import OrphanageData from './pages/CreateOrphanage/OrphanageData';
 
+const { Navigator, Screen } = createStackNavigator();
+
 export default function Routes() {
-    return (
-        <NavigationContainer>
-            <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5'} }}>
-                <Screen 
-                    name="OrphanagesMap" component={OrphanagesMap}/>
+  return (
+    <NavigationContainer>
+      <Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyle: { backgroundColor: '#f2f3f5' },
+        }}
+      >
+        <Screen name="OrphanagesMap" component={OrphanagesMap} />
 
-                <Screen name="OrphanagesDetail" component={OrphanagesDetail}/>
+        <Screen
+          name="OrphanagesDetail"
+          component={OrphanagesDetail}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Orfanato"/>,
+          }}
+        />
 
-                <Screen name="SelectMapPosition" component={SelectMapPosition}/>
+        <Screen name="SelectMapPosition" component={SelectMapPosition} />
 
-                <Screen name="OrphanageData" component={OrphanageData}/>
-            </Navigator>
-        </NavigationContainer>
-    )
-};
+        <Screen name="OrphanageData" component={OrphanageData} />
+      </Navigator>
+    </NavigationContainer>
+  );
+}
